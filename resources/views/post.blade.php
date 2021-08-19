2da542230682e09895c1a51387d16a451a75cb1d
Laura Keen <laurakeen@lauras-mbp.home>
2021-08-19 09:50:38 +0100

    Test

---
+++ resources/views/post.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<title>Post</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+
+
+  <a href='/'>Go back</a>
+</body>
+
+<html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<title>Blog</title>
+<link rel="stylesheet" href="/app.css">
+<!-- <script src="/app.js"></script> -->
+
+<body>
+  <h1>Blog</h1>
+</body>
+
+<html>
